bf668c9bf036297480f752bbc55cc6b46953c7d3
Adithya B <1999adithya@gmail.com>
2023-03-08 22:57:06 +0530

    db creation to store meta data

--- lambda_handlers/write_to_db.py
+++ lambda_handlers/write_to_db.py
@@ -1,5 +1,18 @@
 import os
+import boto3
+import time 
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['DBNAME'])
+
 
 def put_item(event, context):
     # Adds the meta data of images into DB
+    table.put_item(
+        Item={
+                'original': 'originalTest',
+                'thumbnail': 'thumbnailTest',
+                'timestamp' : str(time.time())
+            }
+        )
     return
