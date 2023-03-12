35b881a02d6f01ea11a7368f8de6075c13e61172
Adithya B <1999adithya@gmail.com>
2023-03-12 21:29:14 +0530

    deleteFile lambda logic

--- lambda_handlers/delete_file.py
+++ lambda_handlers/delete_file.py
@@ -1,3 +1,11 @@
+import boto3
+
+s3_client = boto3.client('s3')
+
 def delete_file(event,context):
     # Deletes the source file from s3
+    response = s3_client.delete_object(
+        Bucket=event['s3']['bucket']['name'],
+        Key=event['s3']['object']['key']
+    )
     return
